dd91c8b0ea5bba618991506a0a39460ff5daec2b
Tanya_02 <tr3520@bk.ru>
2024-10-19 20:50:03 +0300

    Created forms

---
+++ users/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from mailings.forms import StyleFormMixin
+from users.models import User
+
+
+class RegistrationForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+class RecoverForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+class UserForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email", "password", "first_name", "last_name", "avatar", "phone_number", "country")
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = forms.HiddenInput()
+
+
+class UserModerationForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("is_blocked",)
